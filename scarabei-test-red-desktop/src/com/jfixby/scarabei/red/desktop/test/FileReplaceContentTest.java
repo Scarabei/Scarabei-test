5787eedfd3eeacf07eda0ff3fc6bf173308f5dad
J Fixby <github@jfixby.com>
2017-07-15 20:41:51 +0200

    ...

--- scarabei-test-red-desktop/src/com/jfixby/scarabei/red/desktop/test/FileReplaceContentTest.java
+++ scarabei-test-red-desktop/src/com/jfixby/scarabei/red/desktop/test/FileReplaceContentTest.java
@@ -9,6 +9,7 @@ import com.jfixby.scarabei.api.desktop.ScarabeiDesktop;
 import com.jfixby.scarabei.api.file.File;
 import com.jfixby.scarabei.api.file.FilesList;
 import com.jfixby.scarabei.api.file.LocalFileSystem;
+import com.jfixby.scarabei.api.log.L;
 import com.jfixby.scarabei.api.sys.Sys;
 
 public class FileReplaceContentTest {
@@ -17,23 +18,65 @@ public class FileReplaceContentTest {
 
 		ScarabeiDesktop.deploy();
 // final File inputFolder = LocalFileSystem.newFile("I");
-		final File outputFolder = LocalFileSystem.newFile("O");
+		final File outputFolder = LocalFileSystem.newFile("D:\\ScarabeiDart\\ScarabeiDart");
 
 // final FileSystem fs = inputFolder.getFileSystem();
 // fs.copyFolderContentsToFolder(inputFolder, outputFolder);
 
-		final FilesList all = outputFolder.listAllChildren();
+		final FilesList all = outputFolder.listAllChildren(file -> {
+			final String name = file.getName();
+// L.d("name", name);
+			final boolean starts = name.startsWith(".git");
+			if (starts) {
+// L.d("name", name);
+			}
+			return !file.getAbsoluteFilePath().getRelativePath().steps().contains(".git");
+
+		});
+// all.print("all");
+// Sys.exit();
 		final List<File> filesToFix = Collections.newList();
 		for (final File f : all) {
-			final String string = f.readToString();
-			if (string.contains("/home/reyer")) {
+			if (f.getName().contains("reyer")) {
 				filesToFix.add(f);
+				continue;
 			}
-// string = string.replaceAll("/home/reyer/", "home/j/");
-// f.writeString(string);
+
+			if (f.isFile()) {
+				final String string = f.readToString();
+				if (string.contains("reyer") || string.contains("Reyer")) {
+					filesToFix.add(f);
+				}
+				continue;
+			}
+
 		}
 
 		filesToFix.print("filesToFix");
 		Sys.exit();
+
+		for (int i = 0; i < filesToFix.size(); i++) {
+			final File f = filesToFix.getElementAt(i);
+			final String ame = f.getName();
+			if (ame.contains("reyer")) {
+				final String new_name = ame.replaceAll("reyer", "red");
+				L.d("rename", f);
+				L.d("  from", ame);
+				L.d("    to", new_name);
+// f.rename(new_name);
+			}
+			if (f.isFile()) {
+				String string = f.readToString();
+				if (!(string.contains("reyer") || string.contains("Reyer"))) {
+					continue;
+				}
+
+				string = string.replaceAll("reyer", "red");
+				string = string.replaceAll("Reyer", "Red");
+				L.d("write", f);
+				f.writeString(string);
+			}
+		}
+
 	}
 }
