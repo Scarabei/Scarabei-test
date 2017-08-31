444f9c52d045e7f78583c914e3b4609824325797
J Fixby <github@jfixby.com>
2017-08-31 14:47:08 +0200

    ...

--- scarabei-test-red-desktop/src/com/jfixby/scarabei/examples/reflect/TestMethodCall.java
+++ scarabei-test-red-desktop/src/com/jfixby/scarabei/examples/reflect/TestMethodCall.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import com.jfixby.scarabei.api.json.Json;
 import com.jfixby.scarabei.api.json.JsonString;
 import com.jfixby.scarabei.api.log.L;
-import com.jfixby.scarabei.api.sys.Sys;
 import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
 import com.jfixby.scarabei.red.reflect.CrossLanguageCallAdaptor;
 
@@ -17,35 +16,30 @@ public class TestMethodCall {
 // com.jfixby.scarabei.api.sys.Sys.getSystemInfo();
 
 		final String methodCallString = "{\r\n" + "   \"value\": {\r\n" + "     \"methodName\": {\r\n"
-			+ "       \"value\": \"com.jfixby.scarabei.api.sys.settings.SystemSettings.setStringParameter\",\r\n"
-			+ "            \"type\": \"com.jfixby.scarabei.api.names.ID\"\r\n" + "          },\r\n"
-			+ "          \"arguments\": {\r\n" + "            \"value\": [\r\n" + "              {\r\n"
-			+ "                \"value\": {\r\n" + "                  \"argumentName\": {\r\n"
-			+ "                    \"value\": \"com.daanuu.app.api_daanuu_url\",\r\n"
-			+ "                    \"type\": \"String\"\r\n" + "                  },\r\n"
-			+ "                  \"argumentValue\": {\r\n" + "                    \"value\": \"com.daanuu.app.api_daanuu_url\",\r\n"
-			+ "                    \"type\": \"com.jfixby.scarabei.api.names.ID\"\r\n" + "                  }\r\n"
-			+ "                },\r\n" + "                \"type\": \"MethodCallArgument\"\r\n" + "              },\r\n"
-			+ "              {\r\n" + "                \"value\": {\r\n" + "                  \"argumentName\": {\r\n"
-			+ "                    \"value\": \"https://devel-api.daanuu.com\",\r\n" + "                    \"type\": \"String\"\r\n"
-			+ "                  },\r\n" + "                  \"argumentValue\": {\r\n"
-			+ "                    \"value\": \"https://devel-api.daanuu.com\",\r\n" + "                    \"type\": \"String\"\r\n"
-			+ "                                                                                                                    }\r\n"
-			+ "                                                                                                                  },\r\n"
-			+ "                \"type\": \"MethodCallArgument\"\r\n" + "              }\r\n" + "            ],\r\n"
-			+ "            \"type\": \"List\"\r\n" + "          }\r\n" + "        },\r\n" + "        \"type\": \"MethodCall\"\r\n"
-			+ "                                                                                                        }";
+			+ "       \"value\": \"com.jfixby.scarabei.api.sys.settings.SystemSettings.setIntParameter\",\r\n"
+			+ "       \"type\": \"com.jfixby.scarabei.api.names.ID\"\r\n" + "     },\r\n" + "     \"arguments\": {\r\n"
+			+ "       \"value\": [\r\n" + "         {\r\n" + "           \"value\": {\r\n" + "             \"argumentName\": {\r\n"
+			+ "               \"value\": \"parameterName\",\r\n" + "               \"type\": \"String\"\r\n" + "             },\r\n"
+			+ "             \"argumentValue\": {\r\n" + "               \"value\": \"com.daanuu.app.logger_padding\",\r\n"
+			+ "               \"type\": \"com.jfixby.scarabei.api.names.ID\"\r\n" + "             }\r\n" + "           },\r\n"
+			+ "           \"type\": \"MethodCallArgument\"\r\n" + "         },\r\n" + "         {\r\n" + "\"value\": {\r\n"
+			+ "             \"argumentName\": {\r\n" + "               \"value\": \"parameterValue\",\r\n"
+			+ "               \"type\": \"String\"\r\n" + "             },\r\n" + "             \"argumentValue\": {\r\n"
+			+ "               \"value\": \"102\",\r\n" + "               \"type\": \"int64\"\r\n" + "             }\r\n"
+			+ "           },\r\n" + "           \"type\": \"MethodCallArgument\"\r\n" + "         }\r\n" + "       ],\r\n"
+			+ "       \"type\": \"List\"\r\n" + "     }\r\n" + "   },\r\n" + "   \"type\": \"MethodCall\"\r\n" + "}";
 		final JsonString jsonInput = Json.newJsonString(methodCallString);
-
+// L.stackTraceToString(e)
 		final JsonString jsonResult = CrossLanguageCallAdaptor.processCrossLanguageMethodCall(jsonInput);
 
 		L.d(jsonResult.toString().replaceAll("\n", ""));
+		L.d(jsonResult.toString());
 
 	}
 
 	public static Object test () throws IOException {
-// throw new IOException("hello");
-		return Sys.getSystemInfo();
+		throw new IOException("hello");
+// return Sys.getSystemInfo();
 
 	}
 
