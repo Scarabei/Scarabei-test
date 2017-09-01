82eb7e2f137dc43ec4882abf7e4ea109c4ffe619
J Fixby <github@jfixby.com>
2017-09-01 23:13:06 +0200

    sdv

--- scarabei-test-red-desktop/src/com/jfixby/scarabei/examples/reflect/TestMethodCall.java
+++ scarabei-test-red-desktop/src/com/jfixby/scarabei/examples/reflect/TestMethodCall.java
@@ -2,10 +2,18 @@
 package com.jfixby.scarabei.examples.reflect;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.jfixby.scarabei.api.codecs.Codecs;
+import com.jfixby.scarabei.api.codecs.io.EncodedObject;
 import com.jfixby.scarabei.api.json.Json;
 import com.jfixby.scarabei.api.json.JsonString;
 import com.jfixby.scarabei.api.log.L;
+import com.jfixby.scarabei.api.names.ID;
+import com.jfixby.scarabei.api.names.Names;
+import com.jfixby.scarabei.api.sys.Sys;
 import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
 import com.jfixby.scarabei.red.reflect.CrossLanguageCallAdaptor;
 
@@ -14,6 +22,21 @@ public class TestMethodCall {
 	public static void main (final String[] args) throws ReflectiveOperationException {
 		ScarabeiDesktop.deploy();
 // com.jfixby.scarabei.api.sys.Sys.getSystemInfo();
+
+		final HashMap<ID, Object> mapping = new HashMap<>();
+		final HashMap mpp = new HashMap();
+		mpp.put("d", new ArrayList());
+		mapping.put(Names.newID("k1"), "sdf");
+		mapping.put(Names.newID("k2"), mpp);
+		mapping.put(Names.newID("k3"), new ArrayList());
+		EncodedObject json = Codecs.encode(mapping);
+		final JsonString jsonString = Json.serializeToString(json);
+		L.d("jsonString", jsonString);
+		json = Json.deserializeFromString(EncodedObject.class, jsonString);
+		final Map<ID, String> map = Codecs.decode(json);
+		L.d("input", mapping);
+		L.d("output", map);
+		Sys.exit();
 
 		final String methodCallString = "{\r\n" + "   \"value\": {\r\n" + "     \"methodName\": {\r\n"
 			+ "       \"value\": \"com.jfixby.scarabei.api.sys.settings.SystemSettings.setIntParameter\",\r\n"
