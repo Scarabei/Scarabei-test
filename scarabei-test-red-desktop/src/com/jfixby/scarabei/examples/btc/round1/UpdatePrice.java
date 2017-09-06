850c0e4fcde3d3bcc0b1a55e8577ecc12cdf7bfd
JFixby <github@jfixby.com>
2017-09-06 22:13:51 +0200

    ...

---
+++ scarabei-test-red-desktop/src/com/jfixby/scarabei/examples/btc/round1/UpdatePrice.java
@@ -0,0 +1,46 @@
+
+package com.jfixby.scarabei.examples.btc.round1;
+
+import java.io.IOException;
+
+import com.jfixby.scarabei.api.collections.Collections;
+import com.jfixby.scarabei.api.collections.List;
+import com.jfixby.scarabei.api.collections.Map;
+import com.jfixby.scarabei.api.file.File;
+import com.jfixby.scarabei.api.file.LocalFileSystem;
+import com.jfixby.scarabei.api.json.Json;
+import com.jfixby.scarabei.api.json.JsonString;
+import com.jfixby.scarabei.api.log.L;
+import com.jfixby.scarabei.api.strings.Strings;
+import com.jfixby.scarabei.api.sys.settings.ExecutionMode;
+import com.jfixby.scarabei.api.sys.settings.SystemSettings;
+import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
+
+public class UpdatePrice {
+	final static String up = "bitcoin\r\n" + "ethereum\r\n" + "bitcoin-cash\r\n" + "ripple\r\n" + "litecoin\r\n" + "nem\r\n"
+		+ "dash\r\n" + "iota\r\n" + "monero\r\n" + "ethereum-classic\r\n" + "neo\r\n" + "omisego";
+
+	public static void main (final String[] args) throws IOException {
+		ScarabeiDesktop.deploy();
+		SystemSettings.setExecutionMode(ExecutionMode.DEMO);
+		final List<String> tags = Strings.split(up, "\r\n");
+
+		final File inputFile = LocalFileSystem.ApplicationHome().child("btc").child("update.json");
+		final String jsonString = inputFile.readToString();
+		final java.util.List<java.util.Map> list = Json.deserializeFromString(java.util.List.class, jsonString);
+		final Map<String, BTC> btx = Collections.newMap();
+		for (final java.util.Map map : list) {
+			final JsonString json_i = Json.serializeToString(map);
+			final BTC btc = Json.deserializeFromString(BTC.class, json_i);
+			btx.put(btc.id, btc);
+		}
+// L.d("btx", btx);
+		for (final String t : tags) {
+			final BTC val = btx.get(t);
+// L.d(t, val.price_eur);
+			L.d(val.price_eur);
+		}
+
+	}
+
+}
